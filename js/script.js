c31614148bf523b9e85a7fbe193cfa6b8b847d61
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-05-06 10:03:37 -0600

    Added rough page structure in html. Started writing Javascript code.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
-let brewerData;
-handleGetData();
-function handleGetData() {
-    $.ajax({
-        url: "https://api.openbrewerydb.org/breweries?by_city=san_diego"
-    }).then(
-        (data) => {
-            brewerData = data;
-            console.log(brewerData)
-        },
-        (error) => {
-            console.log("bad request", error);
-        });
+let brewerData, userInput;
+
+let $inputCity = $("input[type=text]");
+let $brewery = $("#brewery");
+let $type = $("#type");
+let $address = $("#address");
+let $phone = $("#phone");
+let $website = $("#website");
+
+$("form").on("click","i", handleGetData);
+
+function handleGetData(e) {
+  e.preventDefault();
+  userInput = $inputCity.val();
+  if(userInput)
+  $.ajax({
+    url: "https://api.openbrewerydb.org/breweries?by_city=san_diego",
+    // url: "https://api.openbrewerydb.org/breweries?by_city=san%20diego"
+  }).then(
+    (data) => {
+      brewerData = data;
+    },
+    (error) => {
+      console.log("bad request", error);
+    }
+  );
 }
