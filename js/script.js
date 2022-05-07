1731b8d9a41562cc7899a02e4426b27db62c43f3
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-05-06 19:51:56 -0600

    Added table autofill functionality after inputting city.

--- js/script.js
+++ js/script.js
@@ -11,17 +11,40 @@ $("form").on("click","i", handleGetData);
 
 function handleGetData(e) {
   e.preventDefault();
+
   userInput = $inputCity.val();
-  if(userInput)
-  $.ajax({
-    url: "https://api.openbrewerydb.org/breweries?by_city=san_diego",
-    // url: "https://api.openbrewerydb.org/breweries?by_city=san%20diego"
+  userInput = userInput.replace(/\s+/g, "%20"); 
+  //checks userInput for any whitespace and replaces it with URL encoding reference
+  
+  $.ajax({ //Initiate ajax request to openbrewerydb based on city input
+    url: `https://api.openbrewerydb.org/breweries?by_city=${userInput}`,
   }).then(
     (data) => {
       brewerData = data;
+      render();
     },
     (error) => {
       console.log("bad request", error);
     }
   );
+}
+
+function render() {
+  for (let i = 0; i < brewerData.length; i++) {
+    if (i === 0) {
+      $brewery.text(brewerData[i].name);
+      $type.text(brewerData[i].brewery_type);
+      $phone.text(brewerData[i].phone);
+      $website.text(brewerData[i].website_url);
+      $address.text(brewerData[i].street);
+    } else {
+      createNewRow();
+    }
+    
+
+    function createNewRow() {
+      const $newBrewery = $(`<tr><td>${brewerData[i].name}</td><td>${brewerData[i].type}</td><td>${brewerData[i].street}</td><td>${brewerData[i].phone}</td><td><a href="${brewerData[i].website_url}></a>${brewerData[i].name}</td></tr>`)
+      $('#breweries').append($newBrewery)
+    }
+  }
 }
